304e3abb687537b9401befc04be1b387b50c133d
Jagdish <jagdish555u@gmail.com>
2026-06-15 10:55:20 +0530

    Removed css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Cart from './pages/cart'
